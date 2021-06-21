d90d091e1f3f41f785f0486b7739ff9d6416cb56
Marisa131985 <83809809+Marisa131985@users.noreply.github.com>
2021-06-21 22:03:43 +0200

    Add files via upload

---
+++ Change.js
@@ -0,0 +1,9 @@
+function closeMe(){
+    x=document.getElementById("details");
+    x.style.display="none";
+}
+
+function openMe(){
+    x=document.getElementById("details");
+    x.style.display="block";
+}
